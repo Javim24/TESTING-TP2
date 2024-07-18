ad327280e8a800c4239be62d7c74cd740676f9f5
javim24 <javi.mosconi@gmail.com>
2024-07-18 14:19:58 -0300

    Test para encender todos los leds juntos

--- test/test_leds.c
+++ test/test_leds.c
@@ -1,5 +1,4 @@
 /*
-- Prender todos los leds juntos
 - Prender y apagar todos los leds juntos
 - Prender un led, voy a consultar el estado y tiene que figurar como prendido
 - Voy a consultar el estado de  un led apagado y tiene que figurar como apagado
@@ -43,4 +42,10 @@ void test_prender_dos_leds_y_apagar_un_led(void){
     leds_turn_on(7);
     leds_turn_off(3);
     TEST_ASSERT_EQUAL_HEX16(1 << 6, puerto_virtual);
+}
+
+// Prender todos los leds juntos
+void test_prender_todos_leds_juntos(void){
+    leds_turn_on_all();
+    TEST_ASSERT_EQUAL_HEX16(0xFFFF, puerto_virtual);
 }
