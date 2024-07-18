9adbc25c228051b2e461405125575f415d5c80c2
javim24 <javi.mosconi@gmail.com>
2024-07-18 15:18:52 -0300

    Documentación de funciones

--- src/leds.c
+++ src/leds.c
@@ -1,31 +1,102 @@
+/************************************************************************************************
+Copyright (c) 2024, Javier Mosconi <jfmosconi@gmail.com>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+associated documentation files (the "Software"), to deal in the Software without restriction,
+including without limitation the rights to use, copy, modify, merge, publish, distribute,
+sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial
+portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES
+OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+SPDX-License-Identifier: MIT
+*************************************************************************************************/
+
+/**
+ * @file leds.c
+ * @author Javier Mosconi (jfmosconi@gmail.com)
+ * @brief Implementación de funciones públicas y privadas del driver de leds
+ * @date 2024-07-18
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+
+/* === Headers files inclusions =============================================================== */
 #include "leds.h"
 
+
+/* === Macros definitions ====================================================================== */
 #define LED_OFFSET  1
 #define FIRST_BIT   1
 #define ALL_LEDS_OFF 0x0000
 #define ALL_LEDS_ON  0xFFFF
 
+
+/* === Private variable declarations =========================================================== */
+/**
+ * @brief Variable privada que almacena el puerto de leds.
+ * 
+ */
 static led_port_t * puerto_virtual;
 
+
+/* === Private function declarations =========================================================== */
+/**
+ * @brief Genera una máscara para la posición en memoria de cada led.
+ * 
+ * @param led 
+ * @return led_port_t 
+ */
 static led_port_t led_to_mask(uint16_t led){
     return (FIRST_BIT << (led - LED_OFFSET));
 }
 
+/**
+ * @brief Cambia el estado del puerto al que diga el parámetro value.
+ * 
+ * @param value 
+ */
 static void port_set_value(led_port_t value){
     *puerto_virtual = value;
 }
 
+/**
+ * @brief Aplica una máscara para encender un led.
+ * 
+ * @param mask 
+ */
 static void port_apply_on_mask(led_port_t mask){
     *puerto_virtual |= mask;
 }
 
+/**
+ * @brief Aplica una máscara para apagar un led.
+ * 
+ * @param mask 
+ */
 static void port_apply_off_mask(led_port_t mask){
     *puerto_virtual &= ~mask;
 }
 
+/**
+ * @brief Devuelve el estado actual del puerto.
+ * 
+ * @return led_port_t 
+ */
 static led_port_t port_get_value(void){
     return *puerto_virtual;
 }
+
+
+/* === Public function implementation ========================================================== */
 
 void leds_init(led_port_t * puerto){
     puerto_virtual = puerto;
@@ -55,3 +126,5 @@ bool leds_is_led_on(uint16_t led){
 led_port_t leds_led_to_mask(uint16_t led){
     return led_to_mask(led);
 }
+
+/* === End of documentation ==================================================================== */
--- src/leds.h
+++ src/leds.h
@@ -1,25 +1,111 @@
+/************************************************************************************************
+Copyright (c) 2024, Javier Mosconi <jfmosconi@gmail.com>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+associated documentation files (the "Software"), to deal in the Software without restriction,
+including without limitation the rights to use, copy, modify, merge, publish, distribute,
+sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial
+portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES
+OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+SPDX-License-Identifier: MIT
+*************************************************************************************************/
+
 #ifndef LEDS_H
 #define LEDS_H
 
+/**
+ * @file leds.h
+ * @author Javier Mosconi (jfmosconi@gmail.com)
+ * @brief Declaración de funciones públicas para el driver de leds
+ * @date 2024-07-18
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+
+
+/* === Headers files inclusions ================================================================ */
 #include <stdint.h>
 #include <stdbool.h>
 
+/* === Cabecera C++ ============================================================================ */
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* === Public macros definitions =============================================================== */
+
+/* === Public data type declarations =========================================================== */
 typedef uint16_t led_port_t;
 
+/* === Public variable declarations ============================================================ */
+
+/* === Public function declarations ============================================================ */
+
 /**
- * @brief 
+ * @brief Función que inicializa el driver de leds.
  * 
  * @param puerto 
  */
 void leds_init(led_port_t * puerto);
 
+/**
+ * @brief Enciende un led.
+ * 
+ * @param led 
+ */
 void leds_turn_on(uint16_t led);
+
+/**
+ * @brief Enciende todos los leds a la vez.
+ * 
+ */
 void leds_turn_on_all(void);
+
+/**
+ * @brief Apaga un led.
+ * 
+ * @param led 
+ */
 void leds_turn_off(uint16_t led);
+
+/**
+ * @brief Apaga todos los leds a la vez.
+ * 
+ */
 void leds_turn_off_all(void);
 
+/**
+ * @brief Consulta si un led está encendido.
+ * 
+ * @param led 
+ * @return true si el led está encendido.
+ * @return false si el led está apagado.
+ */
 bool leds_is_led_on(uint16_t led);
 
+/**
+ * @brief Devuelve el mapeo en memoria a partir de un número de led.
+ * 
+ * @param led 
+ * @return led_port_t 
+ */
 led_port_t leds_led_to_mask(uint16_t led);
 
+/* === End of documentation ==================================================================== */
+
+#ifdef __cplusplus
+}
 #endif
+
+#endif /* LEDS_H */
