4d0777bbbbb66d6a6629684f4efd138482e71c3f
javim24 <javi.mosconi@gmail.com>
2024-07-18 11:46:38 -0300

    Refactorización de código de tests

--- test/test_leds.c
+++ test/test_leds.c
@@ -1,5 +1,4 @@
 /*
-- Voy a prender un led y volver a apagarlo para ver si se apaga
 - Prender dos leds, apagar uno, y ver que solo se apaga el que corresponde y que el otro sigue prendido
 - Prender todos los leds juntos
 - Prender y apagar todos los leds juntos
@@ -12,6 +11,12 @@
 #include "unity.h"
 #include "leds.h"
 
+static uint16_t puerto_virtual;
+
+void setUp(void){
+    leds_init(&puerto_virtual);
+}
+
 // Al arrancar el sistema todos los leds deben estar apagados
 void test_todos_los_leds_deben_arrancar_apagados(void){
     uint16_t puerto_virtual = 0xFFFF;
@@ -22,18 +27,12 @@ void test_todos_los_leds_deben_arrancar_apagados(void){
 
 // Después de arrancar el sistema, con todos los leds apagados, voy a prender un led cualquiera
 void test_prender_un_solo_led(void){
-    uint16_t puerto_virtual;
-
-    leds_init(&puerto_virtual);
     leds_turn_on(3);
     TEST_ASSERT_EQUAL_HEX16(1 << 2, puerto_virtual);
 }
 
 // Voy a prender un led y volver a apagarlo para ver si se apaga
 void test_prender_apagar_un_solo_led(void){
-    uint16_t puerto_virtual;
-
-    leds_init(&puerto_virtual);
     leds_turn_on(3);
     leds_turn_off(3);
     TEST_ASSERT_EQUAL_HEX16(0x0000, puerto_virtual);
